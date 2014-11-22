3465474b0c59e044ba0b8436995b3e2e461b2694
Eoin Houlihan <ehoulih@tcd.ie>
2014-11-22 12:49:15 +0000

    Use html5Mode for URLS. No more hashes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ angular.module('spServicesApp', [
   'ngRoute',
   'spServicesApp.controllers'
 ]).
-  config(['$routeProvider', function($routeProvider) {
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/', { templateUrl: 'partials/services.html', controller: 'ServicesCtrl' });
     $routeProvider.otherwise({ redirectTo: '/' });
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
 }]);
